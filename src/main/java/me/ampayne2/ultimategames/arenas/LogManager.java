2eb40416092d3db74458f78f69e454c369bf1768
Austin Payne <payneaustin5@gmail.com>
2013-09-01 02:05:16 -0500

    Merge error :(

--- src/main/java/me/ampayne2/ultimategames/arenas/LogManager.java
+++ src/main/java/me/ampayne2/ultimategames/arenas/LogManager.java
@@ -1,10 +1,10 @@
 package me.ampayne2.ultimategames.arenas;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
@@ -16,23 +16,63 @@ import me.ampayne2.ultimategames.database.tables.BlockChangeTable;
 
 public class LogManager {
 
+    private boolean loaded = false;
     private UltimateGames ultimateGames;
     private Integer logTask;
     private Map<Arena, Integer> rollbackTask = new HashMap<Arena, Integer>();
     private List<BlockChange> pendingChanges = new ArrayList<BlockChange>();
     private List<BlockChange> savingChanges = new ArrayList<BlockChange>();
     private Boolean logging = false;
+    private static final long SAVE_DELAY = 0L;
+    private static final long SAVE_PERIOD = 5L;
+    private static final long ROLLBACK_DELAY = 0L;
+    private static final long ROLLBACK_PERIOD = 50L;
+    private static final int CHANGES_PER_ITERATION = 200;
 
     public LogManager(UltimateGames ultimateGames) {
         this.ultimateGames = ultimateGames;
+    }
+
+    public boolean load() {
         startLogSaving();
+        for (Arena arena : ultimateGames.getArenaManager().getArenas()) {
+            startRollingBack(arena);
+        }
+        this.loaded = true;
+        return true;
+    }
+
+    public boolean reload() {
+        stopLogSaving();
+        logging = Boolean.valueOf(false);
+        startLogSaving();
+        for (Arena arena : ultimateGames.getArenaManager().getArenas()) {
+            stopRollingBack(arena);
+            startRollingBack(arena);
+        }
+        loaded = true;
+        return true;
+    }
+
+    public void unload() {
+        stopLogSaving();
+        for (Arena arena : ultimateGames.getArenaManager().getArenas()) {
+            stopRollingBack(arena);
+        }
+        pendingChanges.clear();
+        savingChanges.clear();
+        loaded = false;
+    }
+
+    public boolean isLoaded() {
+        return loaded;
     }
 
     public void logBlockChange(Arena arena, Material material, byte data, Location location) {
         pendingChanges.add(new BlockChange(arena, material, data, location));
     }
 
-    public void logBlockChanges(Arena arena, Set<Block> blocks) {
+    public void logBlockChanges(Arena arena, Collection<Block> blocks) {
         for (Block block : blocks) {
             pendingChanges.add(new BlockChange(arena, block.getType(), block.getData(), block.getLocation()));
         }
@@ -52,7 +92,7 @@ public class LogManager {
                         byte data = blockChange.data;
                         Location location = blockChange.location;
                         if (!((material == Material.WOODEN_DOOR || material == Material.IRON_DOOR_BLOCK) && (data & 0x8) == 0x8)) {
-                            String gameName = arena.getGame().getGameDescription().getName();
+                            String gameName = arena.getGame().getName();
                             String arenaName = arena.getName();
                             double x = location.getX();
                             double y = location.getY();
@@ -68,7 +108,7 @@ public class LogManager {
                                 table.material = material.name();
                                 table.data = data;
                                 ultimateGames.getDatabaseManager().getDatabase().save(table);
-                                System.out.println("Saved Block");
+                                ultimateGames.getMessageManager().debug("Logged block of material " + table.material + " in arena " + table.arenaName + " of game " + table.gameName);
                             }
                         }
                     }
@@ -76,12 +116,14 @@ public class LogManager {
                     logging = false;
                 }
             }
-        }, 0L, 5L);
+        }, SAVE_DELAY, SAVE_PERIOD);
     }
 
     public void stopLogSaving() {
-        Bukkit.getScheduler().cancelTask(logTask);
-        logTask = null;
+        if (logTask != null) {
+            Bukkit.getScheduler().cancelTask(logTask);
+            logTask = null;
+        }
     }
 
     public void rollbackArena(Arena arena) {
@@ -94,13 +136,13 @@ public class LogManager {
         rollbackTask.put(arena, Bukkit.getScheduler().scheduleSyncRepeatingTask(ultimateGames, new Runnable() {
             @Override
             public void run() {
-                List<BlockChangeTable> changes = ultimateGames.getDatabaseManager().getDatabase().select(BlockChangeTable.class).where().equal("gameName",
-                        arena.getGame().getGameDescription().getName()).and().equal("arenaName", arena.getName()).execute().find();
+                List<BlockChangeTable> changes = ultimateGames.getDatabaseManager().getDatabase().select(BlockChangeTable.class).where().equal("gameName", arena.getGame().getName()).and().equal(
+                        "arenaName", arena.getName()).execute().find();
                 if (changes.isEmpty()) {
                     stopRollingBack(arena);
                 } else {
-                    for (int i = 0; i < 50; i++) {
-                        if ((changes.size() <= i)) {
+                    for (int i = 0; i < CHANGES_PER_ITERATION; i++) {
+                        if (!(changes.size() > i)) {
                             return;
                         }
                         BlockChangeTable entry = changes.get(i);
@@ -116,16 +158,18 @@ public class LogManager {
                             }
                         }
                         ultimateGames.getDatabaseManager().getDatabase().remove(
-                                ultimateGames.getDatabaseManager().getDatabase().select(BlockChangeTable.class).where().equal("gameName", arena.getGame().getGameDescription().getName()).and().equal(
-                                        "arenaName", arena.getName()).and().equal("x", entry.x).and().equal("y", entry.y).and().equal("z", entry.z).execute().findOne());
+                                ultimateGames.getDatabaseManager().getDatabase().select(BlockChangeTable.class).where().equal("gameName", arena.getGame().getName()).and().equal("arenaName",
+                                        arena.getName()).and().equal("x", entry.x).and().equal("y", entry.y).and().equal("z", entry.z).execute().findOne());
                     }
                 }
             }
-        }, 0L, 100L));
+        }, ROLLBACK_DELAY, ROLLBACK_PERIOD));
     }
 
     public void stopRollingBack(Arena arena) {
-        Bukkit.getScheduler().cancelTask(rollbackTask.get(arena));
-        rollbackTask.remove(arena);
+        if (rollbackTask.containsKey(arena)) {
+            Bukkit.getScheduler().cancelTask(rollbackTask.get(arena));
+            rollbackTask.remove(arena);
+        }
     }
 }
