3624ca7613007824f65d9501f30cf8e272cb7830
Austin Payne <payneaustin5@gmail.com>
2013-11-06 20:09:07 -0600

    Added simple to use streaks

---
+++ src/main/java/me/ampayne2/ultimategames/players/streaks/StreakAction.java
@@ -0,0 +1,65 @@
+/*
+ * This file is part of UltimateGames.
+ *
+ * Copyright (c) 2013-2013, UltimateGames <http://github.com/ampayne2/>
+ *
+ * UltimateGames is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * UltimateGames is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with UltimateGames.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package me.ampayne2.ultimategames.players.streaks;
+
+import me.ampayne2.ultimategames.players.ArenaPlayer;
+
+/**
+ * Performs an action when a streak reaches the required count.
+ */
+public abstract class StreakAction {
+    private final int requiredKills;
+    private boolean triggered = false;
+
+    public StreakAction(int requiredKills) {
+        this.requiredKills = requiredKills;
+    }
+
+    /**
+     * Gets the kills required for this StreakAction to be performed.
+     *
+     * @return The number of kills.
+     */
+    public int getRequiredKills() {
+        return requiredKills;
+    }
+
+    /**
+     * Checks to see if the StreakAction has already been performed.
+     *
+     * @return True if it has already been performed, else false.
+     */
+    public boolean hasBeenTriggered() {
+        return triggered;
+    }
+
+    /**
+     * Sets the StreakAction's triggered status.
+     *
+     * @param triggered Whether the StreakAction should be triggered.
+     */
+    public void setTriggered(boolean triggered) {
+        this.triggered = triggered;
+    }
+
+    /**
+     * Performs the action.
+     */
+    public abstract void perform(ArenaPlayer player);
+}
