0da144324331a293fb5d5b872e3f17ff0811d2c4
William Lebel <lebelwilliam@gmail.com>
2013-12-25 15:22:40 -0500

    Add the InventoryOpenEvent.

--- src/main/java/me/ampayne2/ultimategames/utils/IconMenu.java
+++ src/main/java/me/ampayne2/ultimategames/utils/IconMenu.java
@@ -1,3 +1,21 @@
+/*
+ * This file is part of UltimateGames.
+ *
+ * Copyright (c) 2013-2013, UltimateGames <http://github.com/ampayne2/>
+ *
+ * UltimateGames is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * UltimateGames is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with UltimateGames.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package me.ampayne2.ultimategames.utils;
 
 import java.util.Arrays;
@@ -9,6 +27,7 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryOpenEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
@@ -56,6 +75,13 @@ public class IconMenu implements Listener {
         plugin = null;
         optionNames = null;
         optionIcons = null;
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = false)
+    void onInventoryOpen(InventoryOpenEvent event) {
+        if (event.getInventory().getName().equals(name)) {
+            event.setCancelled(false);
+        }
     }
 
     @EventHandler(priority=EventPriority.MONITOR)
